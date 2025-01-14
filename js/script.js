cc367d8974686d7f1f1cbfc84e03d53ceb2db9ef
Githubshakil <shakilmahmud007@gmail.com>
2025-01-14 19:47:23 +0600

    update

--- js/script.js
+++ js/script.js
@@ -7,6 +7,25 @@ const cells = Array(9).fill(null);
 let currentPlayer = 'X';
 let gameActive = true;
 
+
+
+const checkWinner = () => {
+    const winningCombination = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 4, 8],
+        [0, 4, 8], [2, 4, 6]
+    ];
+
+    for (const combination of winningCombination){
+        const [a, b, c] = combination;
+        if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]){
+            return cells[a]
+        }
+    }
+    return cells.includes(null) ? null : 'Draw'
+};
+
+
 const handleClick = (e) => {
     const cellIndex = e.target.dataset.index;
 
